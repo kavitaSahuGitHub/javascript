8ad9659982f5b4455b824d7af41cde55e54cf875
kavitaSahuGitHub <kavita2487.sahu@gmail.com>
2023-05-06 22:41:16 +0530

    commiting changes

---
+++ 03_typeOf.js
@@ -0,0 +1,39 @@
+//variable declaration
+console.log("------------bankName-----------------------");
+var bankName;
+console.log("Type of bankname before initilization is: " +typeof bankName);
+console.log("Value of bankname before initilization: " + bankName);
+
+//value assignment
+bankName='SBI BANK';
+console.log("Value of bankname after initilization: " +bankName);
+console.log("Type of bankname after initilization: " +typeof bankName);
+console.log("------------marks-----------------------");
+var marks;
+console.log("Type of marks before initialization:: " +typeof marks);
+console.log("Value of marks before initilization: " + marks);
+marks='90%';
+console.log("Type of marks after initialization: " +typeof marks);
+console.log("Value of marks after initilization: " + marks);
+
+console.log("------------isWorking-----------------------");
+//isWorking
+var isWorking;
+console.log("Type of isWorking before initialization: " +typeof isWorking);
+console.log("Value of isWorking before initilization: " + isWorking);
+isWorking=true;
+console.log("Type of isWorking after initialization: " +typeof isWorking);
+console.log("Value of isWorking after initilization: " + isWorking);
+console.log("------------totalCount-----------------------");
+//totalCount
+var totalCount;
+console.log("Type of totalCount before initialization: " +typeof totalCount);
+console.log("Value of totalCount before initilization: " + totalCount);
+var totalCount='Hundred and Seven';
+console.log("Type of totalCount after initialization: " +typeof totalCount);
+console.log("Value of totalCount after initilization: " + totalCount);
+// totalCount new value 107
+
+totalCount=107;
+console.log("Type of totalCount after new value: " +typeof totalCount);
+console.log("Value of totalCount after new value: " + totalCount);
---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log('kavita sahu');
+console.log('abc Hinjewadi Pune ')
+var firstName ="kavita sahu";
+console.log(firstName);
+var age;//var declaration
+age=22;//variable initialization
+console.log(age);
+var holder;
+console.log(holder);
+holder="My spec";
+console.log(holder);
